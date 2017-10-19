53721157910aac8fcee4cdcf4a8ac06df622c22c
Povilas Korop <povilas@webcoderpro.com>
2017-10-19 10:50:48 +0300

    Some cleanup

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::resource('companies', 'CompaniesController');
+    Route::get('companies', 'CompaniesController@index')->name('companies.index');
 });
