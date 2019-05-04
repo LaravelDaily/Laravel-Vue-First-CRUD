7bde348cad327c50517087ff3b51dbd81b762721
Daniyal Javani <daniyal.javani@gmail.com>
2019-05-04 15:32:16 +0430

    add auth to api with passport

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
+Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.', 'middleware' => 'auth:api'], function () {
     Route::resource('companies', 'CompaniesController', ['except' => ['create', 'edit']]);
 });
